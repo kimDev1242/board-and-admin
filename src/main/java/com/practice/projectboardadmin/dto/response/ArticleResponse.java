e8a4221064ca86cf1b9b0aca6033adcf29a78ce4
kimdev <kimdev1242@gmail.com>
2023-10-14 11:10:25 +0900

    게시판 관리 페이지 구현
    게시판 관리 페이지 구현

---
+++ src/main/java/com/practice/projectboardadmin/dto/response/ArticleResponse.java
@@ -0,0 +1,30 @@
+package com.practice.projectboardadmin.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.practice.projectboardadmin.dto.ArticleDto;
+import com.practice.projectboardadmin.dto.UserAccountDto;
+
+import java.time.LocalDateTime;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record ArticleResponse(
+        Long id,
+        UserAccountDto userAccount,
+        String title,
+        String content,
+        LocalDateTime createdAt
+) {
+
+    public static ArticleResponse of(Long id, UserAccountDto userAccount, String title, String content, LocalDateTime createdAt) {
+        return new ArticleResponse(id, userAccount, title, content, createdAt);
+    }
+
+    public static ArticleResponse withContent(ArticleDto dto) {
+        return ArticleResponse.of(dto.id(), dto.userAccount(), dto.title(), dto.content(), dto.createdAt());
+    }
+
+    public static ArticleResponse withoutContent(ArticleDto dto) {
+        return ArticleResponse.of(dto.id(), dto.userAccount(), dto.title(), null, dto.createdAt());
+    }
+
+}
