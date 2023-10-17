9e295d3f596f362c7873ad1666bad59972d730f6
kimdev <kimdev1242@gmail.com>
2023-10-17 19:58:47 +0900

    방문자 수 집계
    방문자 수 집계

--- src/test/java/com/practice/projectboardadmin/controller/MainControllerTest.java
+++ src/test/java/com/practice/projectboardadmin/controller/MainControllerTest.java
@@ -2,7 +2,9 @@ package com.practice.projectboardadmin.controller;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.practice.projectboardadmin.config.GlobalControllerConfig;
 import com.practice.projectboardadmin.config.SecurityConfig;
+import com.practice.projectboardadmin.config.TestSecurityConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +17,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("View 루트 컨트롤러")
-@Import(SecurityConfig.class)
+@Import({TestSecurityConfig.class, GlobalControllerConfig.class})
 @WebMvcTest(MainController.class)
 class MainControllerTest {
 
